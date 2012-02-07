844fed2ebce660d51d385d5c2eb643abdc048a77
artem <artem.slabodnik@gmail.com>
2012-02-07 17:15:17 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ DemoApp::Application.routes.draw do
   resources :microposts
 
   resources :users
+	root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
